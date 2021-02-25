324c3bdd6370d8bdf53162d8b1079cea0d90f2ac
Grubalub <69207566+Grubalub@users.noreply.github.com>
2021-02-25 11:30:26 +0000

    Update ai.go

--- ai.go
+++ ai.go
@@ -8,34 +8,33 @@ import (
 type AI interface {
 	Bet() int
 	Play(hand []deck.Card, dealer deck.Card) Move
-	Result(hand [][]deck.Card, dealer []deck.Card)
+	Results(hand [][]deck.Card, dealer []deck.Card)
 }
 
-type dealerAI struct {}
+type dealerAI struct{}
 
 func (ai dealerAI) Bet() int {
-	// nothing
+	// noop
 	return 1
 }
 
 func (ai dealerAI) Play(hand []deck.Card, dealer deck.Card) Move {
 	dScore := Score(hand...)
-		if dScore() <= 16 || (dScore() == 17 && Soft(hand...)) {
-	return MoveHit
-		}
-			return MoveStand
-		}
-
-
+	if dScore <= 16 || (dScore == 17 && Soft(hand...)) {
+		return MoveHit
+	}
+	return MoveStand
+}
 
 func (ai dealerAI) Results(hand [][]deck.Card, dealer []deck.Card) {
-// nothing
+	// noop
 }
 
 func HumanAI() AI {
 	return humanAI{}
 }
-type HhmanAI struct{}
+
+type humanAI struct{}
 
 func (ai humanAI) Bet() int {
 	return 1
@@ -43,28 +42,24 @@ func (ai humanAI) Bet() int {
 
 func (ai humanAI) Play(hand []deck.Card, dealer deck.Card) Move {
 	for {
-	fmt.Println("Players:", hand)
-	fmt.Println("Dealer:", dealer)
-	fmt.Println("What do you want to do? (H)it, (S)tand")
+		fmt.Println("Player:", hand)
+		fmt.Println("Dealer:", dealer)
+		fmt.Println("What will you do? (h)it, (s)tand")
 		var input string
-	fmt.Scanf("%s\n", &input)
-	switch input {
-	case "h":
-		return MoveHit
-	case "s":
-		return MoveStand
-	default:
-		fmt.Println("Invald option:", input)
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			return MoveHit
+		case "s":
+			return MoveStand
+		default:
+			fmt.Println("Invalid option:", input)
+		}
 	}
 }
 
-func (ai humanAI) Results(hand [][]deck.Card, dealer[]deck.Card) {
+func (ai humanAI) Results(hand [][]deck.Card, dealer []deck.Card) {
 	fmt.Println("==FINAL HANDS==")
-	fmt.Println("Players:", hand)
+	fmt.Println("Player:", hand)
 	fmt.Println("Dealer:", dealer)
 }
-
-
-// filler for later 
-
-
